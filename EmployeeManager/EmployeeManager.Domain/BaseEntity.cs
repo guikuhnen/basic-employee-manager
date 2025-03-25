83672dc9b5965f0685b3c67e37f958ebac506fab
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2025-03-25 13:28:13 -0300

    Adicionando projetos da arquitetura.

---
+++ EmployeeManager/EmployeeManager.Domain/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Domain
+{
+	internal abstract class BaseEntity
+	{
+		[Key]
+		public long Id { get; set; }
+	}
+}
---
+++ EmployeeManager/EmployeeManager.Domain/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Domain.Models
+{
+	[Table("Employees")]
+	internal class Employee : BaseEntity
+	{
+		[Display(Name = "First Name"), StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1} characters.")]
+		public required string FirstName { get; set; }
+
+		[Display(Name = "Last Name"), StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1} characters.")]
+		public required string LastName { get; set; }
+
+		[DataType(DataType.EmailAddress), EmailAddress(ErrorMessage = "Enter a valid e-mail.")]
+		public required string Email { get; set; }
+
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.None), Display(Name = "Document Number"), Length(6, 20, ErrorMessage = "{0} size should be between {2} and {1} characters.")]
+		public required int DocumentNumber { get; set; }
+
+		// Manager
+		// Role
+
+		[DataType(DataType.Password)]
+		public required string Password { get; set; }
+
+		public string? RefreshToken { get; set; }
+
+		public DateTime RefreshTokenExpiryTime { get; set; }
+
+		[Display(Name = "Birth Date"), DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}"), Required(AllowEmptyStrings = false, ErrorMessage = "Inform {0}.")]
+		public required DateTime BirthDate { get; set; }
+
+		// Validate if is a minor
+	}
+}
