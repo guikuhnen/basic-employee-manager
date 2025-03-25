1d506d46bd7ad788e09cdc80b9c5c6ab45e6b6af
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2025-03-25 12:54:50 -0300

    Criando estrutura básica do projeto back-end.

---
+++ EmployeeManager/EmployeeManager/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManager.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
